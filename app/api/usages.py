5b9c6560ba426415377d7806cf0a887b558d16b4
Fred Machuga <fred@coderite.net>
2020-06-22 11:51:31 -0400

    BROKEN: added tags, usages and started loot types

--- app/api/usages.py
+++ app/api/usages.py
@@ -3,27 +3,27 @@ import xml.etree.ElementTree as ET
 def search():
     from xml.dom import minidom
     mydoc = minidom.parse('./dayzOffline.chernarusplus/cfglimitsdefinition.xml')
-    items = mydoc.getElementsByTagName('category')
+    items = mydoc.getElementsByTagName('usage')
     return [item.attributes['name'].value for item in sorted(items, key=lambda x: x.attributes['name'].value)]
 
 def post(value):
     if value.lower() in search():
-        return 'Server Error', 500, {'x-error': 'category already exists'}
+        return 'Server Error', 500, {'x-error': 'usage already exists'}
 
     import xml.etree.ElementTree as ET
     tree = ET.parse('./dayzOffline.chernarusplus/cfglimitsdefinition.xml')
     root = tree.getroot()
     container = root.find('categories')
-    element = container.makeelement('category', {'name':value.lower()})
+    element = container.makeelement('usage', {'name':value.lower()})
     container.append(element)
     tree.write('./dayzOffline.chernarusplus/cfglimitsdefinition.xml')
     return 'Success', 200
 
 def delete(value):
-    # TODO: Remove all usages of category in other files
+    # TODO: Remove all usages of usage in other files
 
     if not value.lower() in search():
-        return 'Not Found', 404, {'x-error': 'category does not exist'}
+        return 'Not Found', 404, {'x-error': 'usage does not exist'}
 
     tree = ET.parse('./dayzOffline.chernarusplus/cfglimitsdefinition.xml')
     root = tree.getroot()
