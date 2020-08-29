8344aa8f0f2120526fc26e668fa77085258c41eb
ADSHQ\E0175MD <fmachuga@applieddefense.com>
2020-08-29 12:01:19 -0400

    Moved Python into its own repo. Made xml files top level

--- app/api/CategoryService.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from api.BaseService import BaseService
-
-@dataclass(init=True,frozen=True)
-class CategoryService(BaseService):
-    file_name='./dayzOffline.chernarusplus/cfglimitsdefinition.xml'
-    container_name='categories'
-    item_name='category'
